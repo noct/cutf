4f275cbbc59ab7079641299b0e34e5bc6cad0a53
Tarmo Pikaro <tapika@yahoo.com>
2019-06-02 15:52:44 +0300

    Missing file

---
+++ test2.cpp
@@ -0,0 +1,36 @@
+﻿#include "cutf.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define ok(statement)                                       \
+    if( !(statement) )                                      \
+    {                                                       \
+        printf("Failed statement: %s\n", #statement);       \
+        r = 1;                                              \
+    }
+
+int simpleStringTest()
+{
+    const wchar_t* chineseText = L"主体";
+    auto s = widetoutf8(chineseText);
+    size_t r = 0;
+
+    printf("simple string test:  ");
+
+    ok( s.length() == 6 );
+    uint8_t utf8_array[] = { 0xE4, 0xB8, 0xBB, 0xE4, 0xBD, 0x93 };
+
+    for(int i = 0; i < 6; i++)
+        ok(((uint8_t)s[i]) == utf8_array[i]);
+
+    auto ws = utf8towide(s);
+    ok(ws.length() == 2);
+    ok(ws == chineseText);
+
+    if( r == 0 )
+        printf("ok.\n");
+
+    return (int)r;
+}
+
